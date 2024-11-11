55a1d9afd7d6645157b31e2536e56e18fdc846ac
Asterikss <AndreAndreM@proton.me>
2024-11-11 20:50:24 +0100

    switch nvidia driver from 560 to 550.40.07 using mkDriver

--- configuration.nix
+++ configuration.nix
@@ -150,8 +150,34 @@
     enable = true;
   };
   hardware.nvidia = {
-    modesetting.enable = true;
     open = false;
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    nvidiaSettings = true;
+    # package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = let 
+        rcu_patch = pkgs.fetchpatch {
+            url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
+            hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
+        };
+    in config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "550.40.07";
+        sha256_64bit = "sha256-KYk2xye37v7ZW7h+uNJM/u8fNf7KyGTZjiaU03dJpK0=";
+        sha256_aarch64 = "sha256-AV7KgRXYaQGBFl7zuRcfnTGr8rS5n13nGUIe3mJTXb4=";
+        openSha256 = "sha256-mRUTEWVsbjq+psVe+kAT6MjyZuLkG2yRDxCMvDJRL1I=";
+        settingsSha256 = "sha256-c30AQa4g4a1EHmaEu1yc05oqY01y+IusbBuq+P6rMCs=";
+        persistencedSha256 = "sha256-11tLSY8uUIl4X/roNnxf5yS2PQvHvoNjnd2CB67e870=";
+
+        # version = "535.154.05";
+        # sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
+        # sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
+        # openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
+        # settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
+        # persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
+
+        patches = [ rcu_patch ];
+    };
     # https://discourse.nixos.org/t/solved-what-are-the-options-for-hardware-nvidia-package-docs-seem-out-of-date/14251
     # https://nixos.wiki/wiki/Nvidia#CUDA
     # https://nixos.wiki/wiki/Nvidia#Running_the_new_RTX_SUPER_on_nixos_stable
@@ -159,6 +185,7 @@
     # https://discourse.nixos.org/t/issues-with-my-nvidia-gpu-config/35327/2
     # package = config.boot.kernelPackages.nvidiaPackages.stable;
     # https://github.com/vimjoyer/nixconf/blob/main/hosts/laptop/included/nvidia.nix
+    # https://discourse.nixos.org/t/nvidia-4070-super-requires-a-specific-driver/38830
   };
 
   xdg.portal.enable = true;
@@ -178,11 +205,11 @@
     (with pkgs-stable; [
       kitty
       rofi-wayland
+      brave
       waybar
       dunst
       libnotify # dunst depends on it
       swww
-      brave
       wl-clipboard # for global clipbaord in neovim
     ])
 
