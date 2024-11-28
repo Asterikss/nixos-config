11a4f787f32fcaf5433a4678570fe2e3a8c051d8
Asterikss <AndreAndreM@proton.me>
2024-11-28 19:32:41 +0100

    add pickWall script

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -6,6 +6,11 @@
     executable = true;
   };
 
+  home.file.".config/scripts/pickWall.sh" = {
+    source = ./pickWall.sh;
+    executable = true;
+  };
+
   home.file.".config/scripts/wall.sh" = {
     text = ''
       #!/usr/bin/env bash
@@ -13,17 +18,17 @@
       WK_FILE="$HOME/.config/.wk"
 
       if [ ! -f "$WK_FILE" ] ; then
-        image_dir="$HOME/Downloads"
+        image_dir="$HOME/cave/walls"
 
         if [ ! -d "$image_dir" ]; then
-          echo "directory $image_dir does not exist. exiting."
+          notify-send "directory $image_dir does not exist. exiting."
           exit 1
         fi
 
         random_image=$(find "$image_dir" -type f | shuf -n 1)
 
         if [ -z "$random_image" ]; then
-          echo "no files found in $image_dir. exiting."
+          notify-send "no files found in $image_dir. exiting."
           exit 1
         fi
 
---
+++ modules/scripts/pickWall.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+IMAGE_DIR="$HOME/cave/walls"
+
+SELECTED_IMAGE=$(find "$IMAGE_DIR" -type f -printf "%f\n" | rofi -theme purple -dmenu -p "Wall")
+
+if [ -n "$SELECTED_IMAGE" ]; then
+  swww img "$IMAGE_DIR/$SELECTED_IMAGE"
+else
+  notify-send 'No image selected'
+fi
