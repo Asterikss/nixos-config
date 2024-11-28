ed9cb27108bc7617deb26a30e80bffef5fad9b19
Asterikss <AndreAndreM@proton.me>
2024-11-28 01:14:16 +0100

    add comment.nvim

---
+++ nvim/lua/plugins/comment.lua
@@ -0,0 +1,14 @@
+return {
+  'numToStr/Comment.nvim',
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {
+    toggler = {
+      line = 'q',
+      block = 'gbq',
+    },
+    opleader = {
+      line = 'q',
+      block = 'gb',
+    },
+  },
+}
