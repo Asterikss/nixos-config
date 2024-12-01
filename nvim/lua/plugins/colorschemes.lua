1a27aca39367c7e76687cbdc0219864c0fa050b9
Asterikss <AndreAndreM@proton.me>
2024-12-01 12:55:24 +0100

    merge colorshemes to one file

---
+++ nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    'navarasu/onedark.nvim',
+    disabled = true,
+    -- priority = 1000,
+    -- config = function()
+    --   -- vim.cmd.colorscheme 'onedark'
+    --   require('onedark').setup {
+    --     style = 'deep'
+    --   }
+    --   require('onedark').load()
+    -- end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('tokyonight').setup({})
+      vim.cmd[[colorscheme tokyonight-moon]]
+    end,
+  },
+  {
+    'savq/melange',
+    disabled = true,
+  },
+}
