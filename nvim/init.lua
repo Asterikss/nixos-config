f534eae7b8ff9c4b1e34b83394d6a555d75a3386
Asterikss <AndreAndreM@proton.me>
2024-12-01 23:45:00 +0100

    prettify init and move the nixos call to the correct place

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,18 +1,13 @@
--- Get system information
-local system_name = vim.loop.os_uname().sysname
-print(system_name)
-
-if system_name == 'Linux' then
+if vim.loop.os_uname().sysname == 'Linux' then
   local file = io.open('/etc/os-release', 'r')
   if file then
     local content = file:read '*all'
     file:close()
     if string.find(content, 'ID=nixos') then
       vim.g.system_id = 'nixos'
+      package.path = package.path .. ';' .. vim.fn.stdpath('config') .. '-treesitter-parsers/?.lua'
+      require("nvim-treesitter-parsers")
     end
   end
 end
-print(vim.g.system_id)
-package.path = package.path .. ';' .. vim.fn.stdpath('config') .. '-treesitter-parsers/?.lua'
 require("core")
-require("nvim-treesitter-parsers")
