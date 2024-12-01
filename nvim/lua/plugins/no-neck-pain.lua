61e6a42e9fb1eb4b347967d1856de27f3ca63543
Asterikss <AndreAndreM@proton.me>
2024-12-01 12:51:41 +0100

    add no-neck-pain and leap plugins

---
+++ nvim/lua/plugins/no-neck-pain.lua
@@ -0,0 +1,23 @@
+return {
+  'shortcuts/no-neck-pain.nvim',
+  lazy = true,
+  version = '*',
+  keys = {
+    {
+      '<leader><leader>n',
+      mode = 'n',
+      function()
+        vim.cmd('NoNeckPain')
+      end,
+      desc = 'no-neck-pain'
+    }
+  },
+  opts = {
+    width = 130,
+    buffers = {
+      right = {
+        enabled = false,
+      },
+    },
+  },
+}
