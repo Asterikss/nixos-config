e9de02da498585dc25642316aa3527f622416c8f
Andre <AndreAndreM@proton.me>
2024-12-03 17:56:59 +0100

    rewrite treesitter to support non-nixos distros

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,50 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  dev = true,
+  dev = vim.g.system_id == 'nixos',
   event = { 'BufReadPost', 'BufNewFile' },
-  main = 'nvim-treesitter.configs',
-  opts = {
-    auto_install = false,
-    ensure_installed = {},
-    ignore_install = {},
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-      custom_captures = {},
-      disable = {},
-    },
-    incremental_selection = {
-      enable = false,
-      disable = {},
-      keymaps = {
-        init_selection = 'gnn',
-        node_decremental = 'grm',
-        node_incremental = 'grn',
-        scope_incremental = 'grc',
+  config = function ()
+    local ensure_installed_tree = {}
+    if vim.g.system_id ~= 'nixos' then
+      vim.list_extend(ensure_installed_tree, {
+        'python',
+        'rust',
+        'c',
+        'cpp',
+        'lua',
+        'typescript',
+        'tsx',
+        'markdown',
+        'markdown_inline',
+        'vim',
+        'vimdoc',
+        'bash',
+        'yaml',
+      })
+    end
+    require('nvim-treesitter.configs').setup({
+      auto_install = false,
+      ensure_installed = ensure_installed_tree,
+      ignore_install = {},
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+        custom_captures = {},
+        disable = {},
       },
-    },
-    indent = {
-      enable = true,
-      disable = { 'python' },
-    },
-  },
-  -- event = { "BufReadPost", "BufNewFile" },
-  -- dependencies = {
-  --   "nvim-treesitter/nvim-treesitter-textobjects",
-  -- },
+      incremental_selection = {
+        enable = false,
+        disable = {},
+        keymaps = {
+          init_selection = 'gnn',
+          node_decremental = 'grm',
+          node_incremental = 'grn',
+          scope_incremental = 'grc',
+        },
+      },
+      indent = {
+        enable = true,
+        disable = { 'python' },
+      },
+  })
+  end,
 }
