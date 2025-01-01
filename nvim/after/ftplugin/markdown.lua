557b8e9695d5807e101bb7a934678dcec2edb088
Andre <AndreAndreM@proton.me>
2025-01-01 23:19:39 +0100

    update markdown config

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,1 +1,8 @@
 vim.cmd('highlight clear Whitespace')
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 0
+vim.opt_local.shiftwidth = 0
+vim.opt_local.textwidth = 85
+vim.api.nvim_buf_set_keymap(0, 'i', ';', '<End>:<CR><TAB>- ', { noremap = true })
+vim.api.nvim_buf_set_keymap(0, 'i', 'q;', ';', { noremap = true })
+vim.api.nvim_buf_set_keymap(0, 'i', '<C-k>', '<End><CR>- ', { noremap = true })
