32db7e50ba053b8ddf6435600528deb5a992847d
Asterikss <AndreAndreM@proton.me>
2025-01-10 23:54:30 +0100

    change home to standaloneHome for standalone flake

--- hmModules/hmStandalone/flake.nix
+++ hmModules/hmStandalone/flake.nix
@@ -19,7 +19,7 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+        modules = [ ./standaloneHome.nix ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
