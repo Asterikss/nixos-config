99c9fd24d1d5808f5d79bcc946f8b6784b48471a
Andre <AndreAndreM@proton.me>
2025-01-12 23:49:13 +0100

    add a disabling mechanizm for treesitter when dealing with big files

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -47,6 +47,13 @@ return {
         enable = true,
         disable = { 'markdown', 'python' },
       },
+      disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
     })
   end,
 }
