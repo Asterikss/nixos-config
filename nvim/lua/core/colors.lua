fc8ea0341781faf5fc9011856b064e9762a83702
Andre <AndreAndreM@proton.me>
2025-01-23 22:44:39 +0100

    move colors to a seperate file

---
+++ nvim/lua/core/colors.lua
@@ -0,0 +1,37 @@
+vim.api.nvim_set_hl(0, 'TodoOnHold', { fg = '#A19677' })
+vim.api.nvim_set_hl(0, 'TodoUrgent', { fg = '#C7555D' })
+vim.api.nvim_set_hl(0, 'TodoDoLater', { fg = '#CE9904' })
+vim.api.nvim_set_hl(0, 'TodoKindOfDone', { fg = '#84C41E' }) -- 53B9D5
+vim.api.nvim_set_hl(0, 'TodoChecked', { fg = '#6CEA65'}) -- ok
+-- vim.api.nvim_set_hl(0, 'TodoChecked', { fg = '#7BD475'}) -- ok
+
+vim.api.nvim_set_hl(0, 'TodoInProgress', { fg = '#209FBB' })
+vim.api.nvim_set_hl(0, 'TodoUnchecked', { fg = '#B07DD9' }) -- Lavender (floral)
+-- vim.api.nvim_set_hl(0, 'TodoUnchecked', { fg = '#53B9D5' })
+
+-- vim.api.nvim_set_hl(0, 'TodoUnchecked', { fg = '#D9967D' }) -- orangy
+vim.api.nvim_set_hl(0, 'TodoUncertain', { fg = '#692CCB' })
+-- vim.api.nvim_set_hl(0, 'TodoUncertain', { fg = '#6551E3' })
+-- vim.api.nvim_set_hl(0, 'TodoCanceled', { fg = '#808080' })
+-- vim.api.nvim_set_hl(0, 'TodoCanceled', { fg = '#AFA3FF' })
+vim.api.nvim_set_hl(0, 'TodoCanceled', { fg = '#888889' })
+
+-- 53B9D5 Aero (kind of done, unchecked, in progress)
+-- 926D09 Bolden Brown
+-- 5587C7 Silver Lake Blue
+-- 2EBA5A Dark Pastel Green
+-- 4E2F8D Tekhelet Dark Purple
+-- 209FBB Blue Green
+-- 2070BB Azul
+-- 692CCB French Violet
+-- AA9900 Satin sheen gold
+-- 4D69E3 Royal Blue
+-- 6CEA65 Screaming Green
+-- ACA57D Sage - Creamy
+-- 808080 Gray
+-- CE9904 Goldenrod
+-- 1F04CE Medium blue
+-- 6551E3 Majorelle Blue
+-- AFA3FF Tropical indigo
+-- 888889 Battleship gray
+-- B07DD9 Lavender (floral)
--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -77,36 +77,3 @@ vim.api.nvim_create_autocmd('TextYankPost', { -- :h vim.highlight.on_yank
   group = highlight_group,
   pattern = '*',
 })
-
-vim.api.nvim_set_hl(0, 'TodoOnHold', { fg = '#A19677' })
-vim.api.nvim_set_hl(0, 'TodoUrgent', { fg = '#C7555D' })
-vim.api.nvim_set_hl(0, 'TodoDoLater', { fg = '#CE9904' })
-vim.api.nvim_set_hl(0, 'TodoKindOfDone', { fg = '#84C41E' }) -- #4D69E3
-vim.api.nvim_set_hl(0, 'TodoChecked', { fg = '#6CEA65' })
-vim.api.nvim_set_hl(0, 'TodoInProgress', { fg = '#209FBB' })
-vim.api.nvim_set_hl(0, 'TodoUnchecked', { fg = '#B07DD9' }) -- Lavender (floral)
--- vim.api.nvim_set_hl(0, 'TodoUnchecked', { fg = '#D9967D' }) -- orangy
-vim.api.nvim_set_hl(0, 'TodoUncertain', { fg = '#692CCB' })
--- vim.api.nvim_set_hl(0, 'TodoUncertain', { fg = '#6551E3' })
--- vim.api.nvim_set_hl(0, 'TodoCanceled', { fg = '#808080' })
--- vim.api.nvim_set_hl(0, 'TodoCanceled', { fg = '#AFA3FF' })
-vim.api.nvim_set_hl(0, 'TodoCanceled', { fg = '#888889' })
-
--- 926D09 Bolden Brown
--- 5587C7 Silver Lake Blue
--- 2EBA5A Dark Pastel Green
--- 4E2F8D Tekhelet Dark Purple
--- 209FBB Blue Green
--- 2070BB Azul
--- 692CCB French Violet
--- AA9900 Satin sheen gold
--- 4D69E3 Royal Blue
--- 6CEA65 Screaming Green
--- ACA57D Sage - Creamy
--- 808080 Gray
--- CE9904 Goldenrod
--- 1F04CE Medium blue
--- 6551E3 Majorelle Blue
--- AFA3FF Tropical indigo
--- 888889 Battleship gray
--- B07DD9 Lavender (floral)
