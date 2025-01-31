ca52782af797e6042c6af77f33fc4ada87b3c8ad
Andre <AndreAndreM@proton.me>
2025-01-31 23:44:52 +0100

    add markdown qo mapping

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -6,3 +6,4 @@ vim.opt_local.textwidth = 85
 vim.api.nvim_buf_set_keymap(0, 'i', ';', '<End>:<CR><TAB>- ', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, 'i', 'q;', ';', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, 'i', '<C-k>', '<End><CR>- ', { noremap = true })
+vim.api.nvim_buf_set_keymap(0, 'i', 'qo', '- [ ] ', { noremap = true })
