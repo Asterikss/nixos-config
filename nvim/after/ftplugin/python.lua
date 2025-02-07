092da1e97ac8997415dc74617f124197215c647c
Andre <AndreAndreM@proton.me>
2025-02-07 23:39:53 +0100

    update ftplugin for python

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,4 +1,6 @@
-vim.api.nvim_buf_set_option(0, 'tabstop', 4)
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 0
+vim.opt_local.shiftwidth = 0
 vim.api.nvim_buf_set_keymap(0, 'i', 'qp', 'print()<left>', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, 'i', '<a-q>p', 'print("")<left><left>', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, 'i', 'qfp', 'print(f"{=}")<left><left><left><left>', { noremap = true })
@@ -6,5 +8,3 @@ vim.api.nvim_buf_set_keymap(0, 'i', 'qffp', 'print(f"}")<left><left><left>', { n
 vim.api.nvim_buf_set_keymap(0, 'i', ';', '<End>:<CR>', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, 'i', 'q;', ';', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, 'x', '<Leader>cp', ':g/print/s/^/#/\r', { noremap = true }) -- comment all lines starting with print
--- https://github.com/mhartington/formatter.nvim
--- https://github.com/VonHeikemen/lsp-zero.nvi
