ca113369bd3c7e75159eb50984e3bd643eb8d12f
Andre <AndreAndreM@proton.me>
2025-03-08 23:48:47 +0100

    update markdown ftplugin

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -3,7 +3,9 @@ vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 0
 vim.opt_local.shiftwidth = 0
 vim.opt_local.textwidth = 85
+vim.opt.formatoptions:remove('l')
 vim.api.nvim_buf_set_keymap(0, 'i', ';', '<End>:<CR><TAB>- ', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, 'i', 'q;', ';', { noremap = true })
-vim.api.nvim_buf_set_keymap(0, 'i', '<C-k>', '<End><CR>- ', { noremap = true })
+vim.api.nvim_buf_set_keymap(0, 'i', '<C-k>', '<End>.<CR>- ', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, 'i', 'qo', '- [ ] ', { noremap = true })
+vim.api.nvim_buf_set_keymap(0, 'i', 'qp', '```python<CR>```<Esc>O', { noremap = true })
