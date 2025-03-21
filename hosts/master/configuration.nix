ee34b44512fd485e245dd283604b4c1231612fbc
Asterikss <AndreAndreM@proton.me>
2025-03-21 17:58:01 +0100

    add ly display manageer and modularize the config

--- hosts/master/configuration.nix
+++ hosts/master/configuration.nix
@@ -1,14 +1,36 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, pkgs-stable, inputs, ... }:
 
 {
+  networking.hostName = "master";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.lono = {
+    isNormalUser = true;
+    description = "lono";
+    extraGroups = [ "networkmanager" "wheel" ];
+    # packages = with pkgs; [
+    #  kdePackages.kate
+    #  thunderbird
+    # ];
+  };
+
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
+      ../../modules/system.nix
+      ../../modules/ly.nix
     ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+
+
 
   # Enable the Flakes feature and the accompanying new nix command-line tool
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -17,7 +39,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "master"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -27,32 +48,15 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Warsaw";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pl_PL.UTF-8";
-    LC_IDENTIFICATION = "pl_PL.UTF-8";
-    LC_MEASUREMENT = "pl_PL.UTF-8";
-    LC_MONETARY = "pl_PL.UTF-8";
-    LC_NAME = "pl_PL.UTF-8";
-    LC_NUMERIC = "pl_PL.UTF-8";
-    LC_PAPER = "pl_PL.UTF-8";
-    LC_TELEPHONE = "pl_PL.UTF-8";
-    LC_TIME = "pl_PL.UTF-8";
-  };
-
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
+
+  # services.displayManager.sddm.enable = false;
+  # services.displayManager.ly.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -85,17 +89,6 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.lono = {
-    isNormalUser = true;
-    description = "lono";
-    extraGroups = [ "networkmanager" "wheel" ];
-    # packages = with pkgs; [
-    #  kdePackages.kate
-    #  thunderbird
-    # ];
-  };
 
   # Install firefox.
   # programs.firefox.enable = true;
@@ -216,8 +209,6 @@
   # https://github.com/zsa/wally/wiki/Linux-install#2-create-a-udev-rule-file
   hardware.keyboard.zsa.enable = true;
 
-  # programs.neovim.enable = true;
-
   environment.systemPackages =
     (with pkgs; [
       brave
@@ -248,7 +239,6 @@
       alsa-utils
       syncthing
       gcolor3
-      rip2
 
       # fonts
       font-awesome
@@ -301,8 +291,6 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
@@ -311,13 +299,5 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
 
 }
---
+++ modules/ly.nix
@@ -0,0 +1,13 @@
+{...}:
+
+{
+  services.displayManager.ly = {
+    enable = true;
+    settings = {
+      animation = "matrix";
+      animation_timeout_sec = 120;
+      asterisk = "!";
+      clear_password = true;
+    };
+  };
+}
