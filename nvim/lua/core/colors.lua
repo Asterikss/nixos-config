359f5e7017b09f9ff3931c838395a33885a57387
Asterikss <AndreAndreM@proton.me>
2025-04-08 00:30:09 +0200

    refactor colors.lua

--- nvim/lua/core/colors.lua
+++ nvim/lua/core/colors.lua
@@ -1,43 +1,17 @@
-vim.cmd [[highlight Whitespace ctermbg=Brown ctermfg=Black guibg=Brown guifg=Black]]
--- vim.api.nvim_set_hl(0, 'CustomBullet', { fg = '#8263FF' })
--- vim.api.nvim_set_hl(0, 'CustomBullet', { fg = '#C7AD4D' })
+vim.api.nvim_set_hl(0, 'CustomBullet1', { fg = '#fe6d73' })
+vim.api.nvim_set_hl(0, 'CustomBullet2', { fg = '#f4acb7' })
+vim.api.nvim_set_hl(0, 'CustomBullet3', { fg = '#ff7b00' })
+vim.api.nvim_set_hl(0, 'CustomBullet4', { fg = '#ffd6ba' })
+vim.api.nvim_set_hl(0, '@custom.setex.heading.2', { fg = '#fe6d73' })
+vim.api.nvim_set_hl(0, '@custom.setex.underline.2', { fg = '#f4acb7' })
 
-vim.api.nvim_set_hl(0, 'TodoOnHold', { fg = '#A19677' })
-vim.api.nvim_set_hl(0, 'TodoUrgent', { fg = '#C7555D' })
-vim.api.nvim_set_hl(0, 'TodoDoLater', { fg = '#CE9904' })
-vim.api.nvim_set_hl(0, 'TodoKindOfDone', { fg = '#84C41E' }) -- 53B9D5
-vim.api.nvim_set_hl(0, 'TodoChecked', { fg = '#6CEA65'}) -- ok
-vim.api.nvim_set_hl(0, 'TodoImportant', { fg = '#FFC800' }) -- Mikado yellow
--- vim.api.nvim_set_hl(0, 'TodoChecked', { fg = '#7BD475'}) -- ok
-
-vim.api.nvim_set_hl(0, 'TodoInProgress', { fg = '#209FBB' })
 vim.api.nvim_set_hl(0, 'TodoUnchecked', { fg = '#B07DD9' }) -- Lavender (floral)
--- vim.api.nvim_set_hl(0, 'TodoUnchecked', { fg = '#53B9D5' })
-
--- vim.api.nvim_set_hl(0, 'TodoUnchecked', { fg = '#D9967D' }) -- orangy
-vim.api.nvim_set_hl(0, 'TodoUncertain', { fg = '#692CCB' })
--- vim.api.nvim_set_hl(0, 'TodoUncertain', { fg = '#6551E3' })
--- vim.api.nvim_set_hl(0, 'TodoCanceled', { fg = '#808080' })
--- vim.api.nvim_set_hl(0, 'TodoCanceled', { fg = '#AFA3FF' })
+vim.api.nvim_set_hl(0, 'TodoChecked', { fg = '#6CEA65'})
+vim.api.nvim_set_hl(0, 'TodoKindOfDone', { fg = '#84C41E' })
+vim.api.nvim_set_hl(0, 'TodoInProgress', { fg = '#209FBB' })
+vim.api.nvim_set_hl(0, 'TodoDoLater', { fg = '#CE9904' })
+vim.api.nvim_set_hl(0, 'TodoOnHold', { fg = '#A19677' })
 vim.api.nvim_set_hl(0, 'TodoCanceled', { fg = '#888889' })
-
--- 53B9D5 Aero (kind of done, unchecked, in progress)
--- 926D09 Golden Brown
--- 5587C7 Silver Lake Blue
--- 2EBA5A Dark Pastel Green
--- 4E2F8D Tekhelet Dark Purple
--- 209FBB Blue Green
--- 2070BB Azul
--- 692CCB French Violet
--- AA9900 Satin sheen gold
--- 4D69E3 Royal Blue
--- 6CEA65 Screaming Green
--- ACA57D Sage - Creamy
--- 808080 Gray
--- CE9904 Goldenrod
--- 1F04CE Medium blue
--- 6551E3 Majorelle Blue
--- AFA3FF Tropical indigo
--- 888889 Battleship gray
--- B07DD9 Lavender (floral)
--- FFC800 Mikado yellow
+vim.api.nvim_set_hl(0, 'TodoUrgent', { fg = '#C7555D' })
+vim.api.nvim_set_hl(0, 'TodoImportant', { fg = '#FFC800' }) -- Mikado yellow
+vim.api.nvim_set_hl(0, 'TodoUncertain', { fg = '#A3C5FF' })
