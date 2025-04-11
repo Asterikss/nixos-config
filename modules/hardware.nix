f761df7abd9ea1fcd72c0a7ba29df66ce93356e8
Asterikss <AndreAndreM@proton.me>
2025-04-11 23:11:31 +0200

    set up a printer (yes!)

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -3,6 +3,23 @@
 {
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.printing.drivers = [
+    pkgs.hplip
+  ];
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "HP-DeskJet4530";
+        location = "Home";
+        deviceUri = "hp:/usb/DeskJet_4530_series?serial=TH61L3D1HH0661"; # from lpinfo -v output
+        model = "drv:///hp/hpcups.drv/hp-deskjet_4530_series.ppd"; # from lpinfo -m | grep -i deskjet
+        ppdOptions = {
+          PageSize = "A4";
+        };
+      }
+    ];
+    ensureDefaultPrinter = "HP-DeskJet4530";
+  };
 
   hardware.bluetooth.enable = true;
   # hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot # https://nixos.wiki/wiki/Bluetooth
