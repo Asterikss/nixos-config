9b5c532f43f07321fde35b1167df2f0af0783565
Asterikss <AndreAndreM@proton.me>
2025-04-14 23:59:30 +0200

    update audio

--- modules/audio.nix
+++ modules/audio.nix
@@ -10,9 +10,12 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
+    # wireplumber.enable = false
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
+  # https://github.com/NixOS/nixpkgs/issues/330606
+  # hardware.alsa.enablePersistence = true
 }
