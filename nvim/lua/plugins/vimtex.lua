ae954f0d0ef4db84a44100ca95c665fa9751798c
Asterikss <AndreAndreM@proton.me>
2025-05-22 23:40:06 +0200

    update vimtex compiler settings

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -4,5 +4,20 @@ return {
   init = function()
     vim.g.vimtex_view_method = 'zathura'
     vim.g.vimtex_compiler_method = 'latexmk'
-  end
+    vim.g.vimtex_compiler_latexmk = {
+      aux_dir = '',
+      out_dir = 'build',
+      callback = 1,
+      continuous = 1,
+      executable = 'latexmk',
+      hooks = {},
+      options = {
+        '-verbose',
+        '-file-line-error',
+        '-synctex=1',
+        '-interaction=nonstopmode',
+        '-shell-escape',
+      },
+    }
+  end,
 }
