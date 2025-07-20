33d025ddf2e71ed63ffa2944e981ed3b2b061cde
Asterikss <AndreAndreM@proton.me>
2025-07-20 14:14:06 +0200

    fix broken ly config (session_log is clueless)

--- modules/ly.nix
+++ modules/ly.nix
@@ -8,6 +8,11 @@
       animation_timeout_sec = 120;
       asterisk = "!";
       clear_password = true;
+      blank_box = true;
+      hide_borders = false;
+      hide_version_string = true;
+      hide_key_hints = true;
+      session_log = ".ly-session.log";
     };
   };
 }
