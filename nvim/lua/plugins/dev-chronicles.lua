aad40fbdf8a45fd115735516d5c9f54e1136ac76
Asterikss <AndreAndreM@proton.me>
2025-07-20 22:50:15 +0200

    add dev-chronicles plugin setup

---
+++ nvim/lua/plugins/dev-chronicles.lua
@@ -0,0 +1,9 @@
+return {
+  'asterikss/dev-chronicles.nvim',
+  dir = '~/projects/dev-chronicles.nvim/master/',
+  opts = {
+    tracked_parent_dirs = { '~/projects/zzpackage/', '~/projects/' },
+    tracked_dirs = { '~/nixos-config/' },
+    min_session_time = 0,
+  },
+}
