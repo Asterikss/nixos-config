a3db35d63373b36c15df621edf49403921cd12ae
Asterikss <AndreAndreM@proton.me>
2025-11-05 19:23:05 +0100

    nvim: add ruff_organize_imports to conform.nvim setup for python formatting

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -17,7 +17,7 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { 'stylua' },
-      python = { 'ruff_format' },
+      python = { 'ruff_organize_imports', 'ruff_format' },
       nix = { 'nixfmt' },
       cpp = { 'clang-format' },
       -- javascript = { 'prettierd', 'prettier', stop_after_first = true },
